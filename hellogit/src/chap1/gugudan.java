de14d346b9371c909aa9d716f34388e32f15abe9
Sera Han <smart.hansera@gmail.com>
2012-04-28 15:41:47 +0900

    소스 재구성

--- hellogit/src/chap1/gugudan.java
+++ hellogit/src/chap1/gugudan.java
@@ -8,18 +8,14 @@ public class gugudan {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("gugudanfrom:");
 
 		Scanner sc = new Scanner(System.in);
 
-		int k = sc.nextInt();
-		
-		printdan();
+		printdan(sc.nextInt());
 	}
 
-	private static void printdan() {
-		int i;
+	private static void printdan(int i) {
 		int j;
 		for (i = 2; i <= 9; i++) {
 			for (j = 1; j <= 9; j++) {
